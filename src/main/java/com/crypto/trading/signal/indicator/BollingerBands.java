3e523871ce5488bdeb14ea22c3b25327a4120448
leandrodalbo <leandro.e.dal.bo@gmail.com>
2024-09-13 16:09:39 +0100

    MACD Indicator + zero cleanup

--- src/main/java/com/crypto/trading/signal/indicator/BollingerBands.java
+++ src/main/java/com/crypto/trading/signal/indicator/BollingerBands.java
@@ -17,26 +17,30 @@ public class BollingerBands {
     private static final int DEFAULT_PERIOD = 20;
     private static final double STD_DEV = 2;
     private final Core core;
-
+    private final ZeroCleaner zeroCleaner;
     @Value("${bbands.period}")
     private int bandsPeriod;
-
     @Value("${bbands.stddev}")
     private double stdDev;
 
-    public BollingerBands(Core core) {
+    public BollingerBands(Core core, ZeroCleaner zeroCleaner) {
         this.core = core;
+        this.zeroCleaner = zeroCleaner;
     }
 
     public Map<String, Double> bands(float[] values) {
         int period = (bandsPeriod != 0) ? bandsPeriod : DEFAULT_PERIOD;
         double upAndDown = (stdDev != 0) ? stdDev : STD_DEV;
 
-        double[] upperBand = new double[values.length - period + 1];
-        double[] middleBand = new double[values.length - period + 1];
-        double[] lowerBand = new double[values.length - period + 1];
+        double[] upperBand = new double[values.length];
+        double[] middleBand = new double[values.length];
+        double[] lowerBand = new double[values.length];
 
         core.bbands(0, values.length - 1, values, period, upAndDown, upAndDown, MAType.Sma, new MInteger(), new MInteger(), upperBand, middleBand, lowerBand);
+
+        upperBand = zeroCleaner.cleanUp(upperBand);
+        middleBand = zeroCleaner.cleanUp(middleBand);
+        lowerBand = zeroCleaner.cleanUp(lowerBand);
 
         return Map.of(UPPER_BAND_KEY, upperBand[upperBand.length - 1], MIDDLE_BAND_KEY, middleBand[middleBand.length - 1], LOWER_BAND_KEY, lowerBand[lowerBand.length - 1]);
     }
