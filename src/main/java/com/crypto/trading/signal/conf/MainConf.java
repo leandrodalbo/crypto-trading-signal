0da10cf9166fc883938b918167c30b116f217729
leandrod <leandro.e.dal.bo@gmail.com>
2024-12-13 23:51:08 +0000

    reducing costs + new certificate

--- src/main/java/com/crypto/trading/signal/conf/MainConf.java
+++ src/main/java/com/crypto/trading/signal/conf/MainConf.java
@@ -24,8 +24,7 @@ public class MainConf implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Origin", String.format("https://%s",allowOrigin));
-        response.setHeader("Access-Control-Allow-Origin", String.format("https://www.%s",allowOrigin));
+        response.setHeader("Access-Control-Allow-Origin", allowOrigin);
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "GET");
         response.setHeader("Access-Control-Max-Age", "3600");
